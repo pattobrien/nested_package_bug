23c799e4f753d760348401d46220dba2f9b24b2c
pattobrien <12474318+pattobrien@users.noreply.github.com>
2023-08-29 11:20:52 -0400

    first commit

---
+++ lib/nested_package_bug.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library;
+
+export 'src/nested_package_bug_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
---
+++ test/nested_package_bug_test.dart
@@ -0,0 +1,16 @@
+import 'package:nested_package_bug/nested_package_bug.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
